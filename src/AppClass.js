6c837f1d61c8f091490b33ce92ae7c5df9823365
Bruno Silva <brunosilvinosilva@gmail.com>
2020-05-28 12:44:01 +0100

    used hook with boolean to change css

--- src/AppClass.js
+++ src/AppClass.js
@@ -4,7 +4,8 @@ import React, {Component} from 'react';
 class App extends Component {
 
   state = {
-    count: 0
+    count: 0,
+    isOn: false 
   }
 
   incrementCount = () => {
@@ -12,11 +13,25 @@ class App extends Component {
       count: this.state.count +  1
     })
   }
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
+  }
   render(){
+
+    const {count, isOn} = this.state
     return (
-      <div className="App">
-        <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
-      </div>
+       <React.Fragment>
+            <h2>Counter</h2>
+            <button onClick={this.incrementCount}> I was clicked {count} times</button>
+            <h2>Toggle Light</h2>
+            <div style={{height: "50px", width: "50px", background: isOn ? "yellow" : "grey"}}
+            onClick={this.toggleLight}>
+
+            </div>
+        </React.Fragment>
     );
   }
 }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -3,15 +3,31 @@ import React, { useState} from 'react';
 const App = () => {
 
     const [count, setCount] = useState(0)
+    const [isOn, setIsOn] = useState(false)
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1)
     }
 
+    const toggleLike = () => {
+        setIsOn(prevIsOn => !prevIsOn)
+    }
+
     return (
-        <div>
+        <React.Fragment>
+            <h2>Counter</h2>
             <button onClick={incrementCount}> I was clicked {count} times</button>
-        </div>
+            <h2>Toggle Light</h2>
+            <div style={{height: "50px", width: "50px", background: isOn ? "yellow" : "grey"}}
+            onClick={toggleLike}>
+
+            </div>
+            {/* <img  style={{height: "50px", width: "50px"}}
+                onClick={toggleLike} src={isOn ? "https://icon.now/highlight/fd0" : "https://icon.now/highlight/aaa"}
+                alt="Flashlight"
+            >
+            </img> */}
+        </React.Fragment>
     )
 }
 
