851d52c15348af0bbec0ee3f04883cf2c73c36e3
Bruno Silva <brunosilvinosilva@gmail.com>
2020-05-28 13:51:39 +0100

    useEffect hooks and how they compare

--- src/AppClass.js
+++ src/AppClass.js
@@ -5,15 +5,29 @@ class App extends Component {
 
   state = {
     count: 0,
-    isOn: false 
+    isOn: false,
+    x: null, 
+    y: null
   }
 
   componentDidMount() {
     document.title = `${this.state.count}`
+    window.addEventListener('mousemove', this.handleMouseMove)
   }
 
   componentDidUpdate() {
     document.title = `${this.state.count}`
+  }
+
+  componentWillMount() {
+    window.removeEventListener('mousemove', this.handleMouseMove)
+  }
+
+  handleMouseMove = event => {
+    this.setState({
+      x: event.pageX,
+      y: event.pageY
+    })
   }
 
   incrementCount = () => {
@@ -33,12 +47,14 @@ class App extends Component {
     return (
        <React.Fragment>
             <h2>Counter</h2>
-            <button onClick={this.incrementCount}> I was clicked {count} times</button>
+              <button onClick={this.incrementCount}> I was clicked {count} times</button>
             <h2>Toggle Light</h2>
-            <div style={{height: "50px", width: "50px", background: isOn ? "yellow" : "grey"}}
-            onClick={this.toggleLight}>
-
-            </div>
+              <div style={{height: "50px", width: "50px", background: isOn ? "yellow" : "grey"}}
+              onClick={this.toggleLight}>
+              </div>
+            <h2>Mouse Position</h2>
+            <p>X position: {this.state.x}</p>
+            <p>Y position: {this.state.y}</p>
         </React.Fragment>
     );
   }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -4,11 +4,24 @@ const App = () => {
 
     const [count, setCount] = useState(0)
     const [isOn, setIsOn] = useState(false)
+    const [mousePosition, setMousePosition] = useState({x: null, y: null})
 
 
     useEffect(() => {
-        document.title = `you have clicked ${count} times`
-    })
+        document.title = `${count}`
+        window.addEventListener('mousemove', handleMouseMove)
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove)
+        }
+    }, [count])
+
+    const handleMouseMove = event => {
+        setMousePosition({
+            x: event.pageX,
+            y: event.pageY
+        })
+    }
 
     const incrementCount = () => {
         setCount(prevCount => prevCount + 1)
@@ -27,6 +40,10 @@ const App = () => {
             onClick={toggleLike}>
 
             </div>
+
+            <h2>Mouse Position</h2>
+            <p>Y position: {JSON.stringify(mousePosition, null, 2)}</p>
+
             {/* <img  style={{height: "50px", width: "50px"}}
                 onClick={toggleLike} src={isOn ? "https://icon.now/highlight/fd0" : "https://icon.now/highlight/aaa"}
                 alt="Flashlight"
