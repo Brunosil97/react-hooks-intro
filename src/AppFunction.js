cced2c890661cd42c829df787b4ddd8689ef8a82
Bruno Silva <brunosilvinosilva@gmail.com>
2020-05-28 16:39:32 +0100

    added geolocation and worked with fetching from internal API and displaying data with hooks

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,68 +1,107 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(initialLocationState);
+  let mounted = true;
 
-    const [count, setCount] = useState(0)
-    const [isOn, setIsOn] = useState(false)
-    const [mousePosition, setMousePosition] = useState({x: 0, y: 0})
-    const [status, setStatus] = useState(navigator.onLine)
+  useEffect(
+    () => {
+      document.title = `You have clicked ${count} times`;
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("online", handleOnline);
+      window.addEventListener("offline", handleOffline);
+      navigator.geolocation.getCurrentPosition(handleGeolocation);
+      const watchId = navigator.geolocation.watchPosition(handleGeolocation);
 
-    useEffect(() => {
-        document.title = `${count}`
-        window.addEventListener('mousemove', handleMouseMove)
-        window.addEventListener('online', handleOnline)
-        window.addEventListener('offline', handleOffline)
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("online", handleOnline);
+        window.removeEventListener("offline", handleOffline);
+        navigator.geolocation.clearWatch(watchId);
+        mounted = false;
+      };
+    },
+    [count]
+  );
 
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
 
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove)
-            window.removeEventListener('online', handleOnline)
-            window.removeEventListener('offline', handleOffline)
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
+
+  const handleMouseMove = event => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    });
+  };
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn);
+  };
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>
+
+      <h2>Toggle Light</h2>
+      <img
+        src={
+          isOn
+            ? "https://icon.now.sh/highlight/fd0"
+            : "https://icon.now.sh/highlight/aaa"
         }
-    }, [count])
+        style={{
+          height: "50px",
+          width: "50px"
+        }}
+        alt="Flashlight"
+        onClick={toggleLight}
+      />
 
-    const handleOnline = () => {
-        setStatus(true)
-    }
+      <h2>Mouse Position</h2>
+      {JSON.stringify(mousePosition, null, 2)}
+      <br />
 
-    const handleOffline = () => {
-        setStatus(false)
-    }
+      <h2>Network Status</h2>
+      <p>
+        You are <strong>{status ? "online" : "offline"}</strong>
+      </p>
 
-    const handleMouseMove = event => {
-        setMousePosition({
-            x: event.pageX,
-            y: event.pageY
-        })
-    }
+      <h2>Geolocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Your speed is {speed ? speed : "0"}</p>
+    </>
+  );
+};
 
-    const incrementCount = () => {
-        setCount(prevCount => prevCount + 1)
-    }
-
-    const toggleLike = () => {
-        setIsOn(prevIsOn => !prevIsOn)
-    }
-
-    return (
-        <React.Fragment>
-            <h2>Counter</h2>
-            <button onClick={incrementCount}> I was clicked {count} times</button>
-            <h2>Toggle Light</h2>
-            <div style={{height: "50px", width: "50px", background: isOn ? "yellow" : "grey"}}
-            onClick={toggleLike}>
-
-            </div>
-
-            <h2>Mouse Position</h2>
-            <p>Y position: {JSON.stringify(mousePosition.y)}</p>
-            <p>X position: {JSON.stringify(mousePosition.x)}</p>
-
-            <h2>Network Status</h2>
-            <p>You are <strong>{status ? "online" : "offline"}</strong></p>
-
-        </React.Fragment>
-    )
-}
-
-export default App
+export default App;
